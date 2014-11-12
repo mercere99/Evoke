85b00bf9eb5c7a7dc64f9e5dab40f59d0bd47cf4
Charles Ofria <charles.ofria@gmail.com>
2014-11-11 21:53:21 -0500

    Linked physics into html interface

--- drivers/html.cc
+++ drivers/html.cc
@@ -1,6 +1,10 @@
 #include <emscripten.h>
 
+#include "tools/config.h"
+#include "tools/callbacks.h"
 #include "kinetic/Kinetic.h"
+#include "geometry/Body2D.h"
+#include "geometry/Physics2D.h"
 #include "../web/Viewport.h"
 
 class EvokeInterface {
@@ -8,6 +12,7 @@ private:
   evoke::Viewport viewport;
   emp::Layer layer_anim;
   emp::Stage stage;
+  emp::cConfig config;
 
 public:
   EvokeInterface()
@@ -24,11 +29,23 @@ public:
   ~EvokeInterface() { ; }
 };
 
+// @CAO Move elsewhere!
+class OrgControl {
+private:
+public:
+  OrgControl() { ; }
+  ~OrgControl() { ; }
+};
 
 EvokeInterface * evoke_interface;
 
 extern "C" int evokeMain()
 {
+  emp::Physics2D< emp::CircleBody2D< OrgControl, int >, OrgControl, int > physics(1000, 1000);
+
+  auto new_org = new emp::CircleBody2D<OrgControl, int>(emp::Circle<int>(emp::Point<int>(123,456), 10), NULL);
+  physics.AddActiveBody(new_org);
+
   evoke_interface = new EvokeInterface();
 
   return 0;
