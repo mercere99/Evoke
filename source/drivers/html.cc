5148ab202231b964ed1388331cbe2fba36064af8
Charles Ofria <charles.ofria@gmail.com>
2015-07-25 05:44:05 -0400

    Shifted from Kinetic to new UI framework.

--- source/drivers/html.cc
+++ source/drivers/html.cc
@@ -7,21 +7,14 @@
 #include "geometry/Physics2D.h"
 
 #include "UI/Animate.h"
+#include "UI/canvas_utils.h"
 #include "UI/keypress.h"
 #include "UI/UI.h"
 
 #include "../main/World.h"
 #include "../organisms/OrgControl.h"
-#include "../webtools/Viewport.h"
-
-// Phasing out...
-#include "kinetic/Kinetic.h"
 
 namespace UI = emp::UI;
-
-namespace evoke {
-  using dViewport = Viewport<dBody, dControl>;
-}
 
 class EvokeInterface {
 private:
@@ -31,127 +24,95 @@ private:
 
   UI::KeypressManager keypress_manager;
 
-  emp::Kinetic::Stage stage;
-  emp::Kinetic::Layer layer_anim;
-  evoke::dViewport viewport;
-  emp::Kinetic::Animation<EvokeInterface> anim_interface;
 public:
   EvokeInterface()
     : doc("emp_base")
-    , anim([this](){ EvokeInterface::Animate2(anim); })
-    , stage(world.width, world.height, "container")
-    , viewport(0, 0, world)
+    , anim([this](){ EvokeInterface::Animate(anim); })
   {    
     // Link keypresses to the proper handlers
-    keypress_manager.AddKeydownCallback(std::bind(&evoke::dViewport::OnKeydown, &viewport, _1));
     keypress_manager.AddKeydownCallback(std::bind(&EvokeInterface::OnKeydown, this, _1));
 
-    // Add a cavas to draw the world.
-    doc << UI::Canvas(world.width, world.height, "pop_view");
+    // Add a canvas to draw the world.
+    doc << UI::Canvas(world.width, world.height, "pop_view") << "<br>";
 
-    // Add a play/pause button.
-    doc.AddButton([this](){
-        anim.ToggleActive();
-        auto & but = doc.Button("play_but");
-        if (anim.GetActive()) but.Label("Pause");
-        else but.Label("Play");
-        but.Update();
-      }, "Play", "play_but");
+    // Add buttons.
+    doc.AddButton([this](){DoPlay();}, "Play", "play_but");
+    doc.AddButton([this](){DoStep();}, "Step", "step_but");
     
     doc.Update();
 
-
-
-    // Link button callbacks to the proper handlers.
-    auto play_cb = new emp::MethodCallback<EvokeInterface>(this, &EvokeInterface::DoPlay);
-    auto step_cb = new emp::MethodCallback<EvokeInterface>(this, &EvokeInterface::DoStep);
-    auto repro_cb = new emp::MethodCallback<EvokeInterface>(this, &EvokeInterface::DoRepro);
-    EM_ASM_ARGS({
-        emp_kinetic.callbacks.play_cb = $0;
-        emp_kinetic.callbacks.step_cb = $1;
-        emp_kinetic.callbacks.repro_cb = $2;
-      }, (int) play_cb, (int) step_cb, (int) repro_cb);
-
-
-    // Initialize organisms in the world.
-    const int base_radius = 6;
-
-    auto org = new evoke::dBody(evoke::dCircle(evoke::dPoint(world.width/2,world.height/2), base_radius), NULL);
+    // Initialize an organism in the middle of the world.
+    const evoke::dPoint mid_point( world.width / 2.0, world.height / 2.0 );
+    const int org_radius = 6;
+    auto org = new evoke::dBody(evoke::dCircle(mid_point, org_radius), NULL);
     world.physics.AddBody(org);
 
-    layer_anim.Add(viewport);
-    stage.Add(layer_anim);
 
-    anim_interface.Setup(this, &EvokeInterface::Animate, layer_anim);
-    anim_interface.Start();
+    // Draw initial state of the world.
+    UI::Draw( doc.Canvas("pop_view"),
+              world.physics.GetSurface(),
+              emp::GetHueMap(360));
   }
 
   ~EvokeInterface() { ; }
 
-  void Animate2(const UI::Animate & anim) {
-  }
-
-  void Animate(const emp::Kinetic::AnimationFrame & frame) {
+  void Animate(const UI::Animate & anim) {
+    // Take a single timestep on the world physics.
     world.physics.Update();
 
     // Test which organisms should replicate.
-    const double avg_repro_time = 10.0; // In seconds
-    double repro_prob = ((double) frame.time_diff) / (avg_repro_time * 1000.0);
-
-    repro_prob = 0.003;
+    double repro_prob = 0.003;
 
     auto & body_set = world.physics.GetBodySet();
-    if (body_set.size())
 
     for (auto * body : body_set) {
-      // Bodies that are reproducing cannot produce a second offspring until they are done.
-      // Bodies under pressure do not produce offspring.
+      // Bodies that are already reproducing or under pressure cannot produce offspring.
       if (body->IsReproducing() || body->GetPressure() > 1.0) continue;
+
       if (world.random.P(repro_prob) || body_set.size() == 1) {
         emp::Angle repro_angle(world.random.GetDouble(2.0 * emp::PI));
         auto * new_body = body->BuildOffspring( repro_angle.GetPoint(0.1) );
-        if (world.random.P(0.95)) {
-          new_body->SetColorID(body->GetColorID());
-        }
-        else {
-          new_body->SetColorID( world.random.GetInt(360) );
-        }
+
+        // For the moment, assume 95% chance of faithful copy.
+        if (world.random.P(0.95)) new_body->SetColorID(body->GetColorID());
+        else new_body->SetColorID( world.random.GetInt(360) );
+
         world.physics.AddBody( new_body );
       }
     }
 
+    UI::Draw( doc.Canvas("pop_view"),
+              world.physics.GetSurface(),
+              emp::GetHueMap(360));
+
   }
 
   void DoPlay() {
-    if (anim_interface.IsRunning()) {
-      anim_interface.Stop();
-      EM_ASM({
-          document.getElementById("play_button").innerHTML = "[P]lay";
-          document.getElementById("step_button").disabled = false;
-        });
+    anim.ToggleActive();
+    auto & play_but = doc.Button("play_but");
+    auto & step_but = doc.Button("step_but");
+
+    if (anim.GetActive()) {
+      play_but.Label("Pause");    // If animation is running, button should read "Pause"
+      step_but.Disabled(true);    // Cannot step animation already running.
     }
     else {
-      anim_interface.Start();
-      EM_ASM({
-          document.getElementById("play_button").innerHTML = "[P]ause";
-          document.getElementById("step_button").disabled = true;
-        });
+      play_but.Label("Play");     // If animation is paused, button should read "Play"
+      step_but.Disabled(false);    // Can step paused animation.
     }
-  }
-  void DoStep() {
-    // Step is only meaningful if the run is paused.
-    if (anim_interface.IsRunning() == false) {
-      world.physics.Update();
-      layer_anim.Draw();
-    }
+
+    play_but.Update();
+    step_but.Update();
   }
 
-  void DoRepro() {
-    evoke::dBody * body = viewport.GetUserBody();
-    if (!body) return;
-    emp::Angle repro_angle(world.random.GetDouble(2.0 * emp::PI));
-    world.physics.AddBody( body->BuildOffspring( repro_angle.GetPoint(0.01) ) );
+  void DoStep() {
+    // NOTE: Step is only meaningful if the run is paused.
+    emp_assert(anim.GetActive() == false);
+
+    world.physics.Update();
+    doc.Canvas("pop_view").Refresh();
   }
+
 
   bool OnKeydown(const emp::html5::KeyboardEvent & evt_info) {
     const int key_code = evt_info.keyCode;
@@ -162,18 +123,37 @@ public:
     case 'P':
       DoPlay();
       break;
-    case 'r':                                     // R => Reproduce
-    case 'R':
-      DoRepro();
-      break;
     case 's':                                     // S => Step
     case 'S':
       DoStep();
       break;
+
+    // case 187:                                     // Plus  (grow)
+    //   if (user_body) user_body->SetTargetRadius(user_body->GetTargetRadius() + 1);
+    //   break;
+    // case 189:                                     // Minus (shrink)
+    //   if (user_body) {
+    //     int body_size = user_body->GetTargetRadius();
+    //     if (body_size > 1) user_body->SetTargetRadius(body_size - 1);
+    //   }
+    //   break;
+    // case 37:                                      // LEFT ARROW (Turn Left)
+    //   if (user_body) user_body->TurnLeft();
+    //   break;
+    // case 38:                                      // UP ARROW (Accellerate)
+    //   if (user_body) user_body->IncSpeed();
+    //   break;
+    // case 39:                                      // RIGHT ARROW (Turn Right)
+    //   if (user_body) user_body->TurnRight();
+    //   break;
+    // case 40:                                      // DOWN ARROW (Breaks)
+    //   if (user_body) user_body->DecSpeed();
+    //   break;
+
     default:
       return_value = false;
     };
-
+    
     return return_value;
   }
 
@@ -181,12 +161,9 @@ public:
 
 EvokeInterface * evoke_interface;
 
-// extern "C" int evokeMain()
 extern "C" int main()
 {  
-  EMP_SETUP();
   UI::Initialize();
-
   evoke_interface = new EvokeInterface();
 
   return 0;
